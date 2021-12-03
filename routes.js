c4538c5fd5a1e9e1e5647c760ed2e9eef4350dfc
Benedek Balint Toth <benedek.toth@bookrkids.hu>
2021-12-03 10:50:16 +0100

    pushing to array instead of overwrite

--- routes.js
+++ routes.js
@@ -3,10 +3,14 @@ const fs = require("fs");
 
 const router = express.Router();
 
+const verificationResults = [];
+
 router.post("/parent-verified", (req, res) => {
   res.json(req.body);
 
-  fs.writeFile( "filename.json", JSON.stringify( req.body ), "utf8", () => {
+  verificationResults.push(req.body);
+
+  fs.writeFile( "filename.json", JSON.stringify( verificationResults ), "utf8", () => {
     console.log("IO performed");
   });
 
